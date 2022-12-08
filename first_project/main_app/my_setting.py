f58b437028eb01433a1f9b1d8189b3df2b5ff924
dong002 <81304010+dong002@users.noreply.github.com>
2022-12-09 08:56:09 +0900

    clear git cache

---
+++ first_project/main_app/my_setting.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dong',
+        'USER': 'root',
+        'PASSWORD': 'youtube',
+        'PORT': 3306,
+        'HOST': '127.0.0.1',
+        'CHARSET': 'utf8'
+    }
+}
